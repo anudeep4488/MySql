36a9c49014f686c854a299a0f344b776f1d258b2
anudeep4488 <anudeepj04@gmail.com>
2026-04-22 19:26:58 +0530

    Time: 1 ms (99.81%) | Memory: 42.8 MB (51.57%) - LeetSync

--- Arrayss/1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ Arrayss/1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,28 +1,29 @@
 class Solution {
-    	 public static int maxScore(String s) {
+    	public int maxScore(String s) {
 
-	        int countOfZeors = 0;
-	        int max = 0;
-	        
-	        for(int i =0 ; i < s.length()-1 ; i++){
-	            if(s.charAt(i)=='0') {
-	            	countOfZeors++;
-	            }
+    int zeros = 0;
+    int ones = 0;
+    int maxDiff = Integer.MIN_VALUE;
 
-	            int countOfOnes = 0;
-	            int sum=0;
-	            for(int j=i+1;j<s.length();j++){
-	                if(s.charAt(j)=='1') {
-	                	countOfOnes++;
-	                }
-	            }
-	            sum = countOfZeors + countOfOnes;
-	            max = Math.max(sum,max);
+    for (int i = 0; i < s.length() - 1; i++) {
 
-	        }
-	        return max;
-	        
-	    }
+        if (s.charAt(i) == '0') {
+            zeros++;
+        } else {
+            ones++;
+        }
 
+        maxDiff = Math.max(maxDiff, zeros - ones);
+    }
+
+    // add last character if it's '1'
+    if (s.charAt(s.length() - 1) == '1') {
+        ones++;
+    }
+
+    return maxDiff + ones;
+}
+
+	       
   
 }
