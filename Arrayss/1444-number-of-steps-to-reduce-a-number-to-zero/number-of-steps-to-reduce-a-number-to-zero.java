a93b0bb6fe39912ac875fcaa1ef23bcb8b40b555
anudeep4488 <anudeepj04@gmail.com>
2026-05-02 18:04:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (17.55%) - LeetSync

--- Arrayss/1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
+++ Arrayss/1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,18 +1,36 @@
 class Solution {
     public int numberOfSteps(int num) {
-        int steps=0;
-        while(num!=0){
-            if(num%2==0){
-                steps++;
-                num=num/2;
-            }
-            else{
-                num=num-1;
-                steps++;
-            }
-        }
-        return steps;
+        return steps(num , 0);
+        // indial steps 0
+
+// using while loop
+        // int steps=0;
+        // while(num!=0){
+        //     if(num%2==0){
+        //         steps++;
+        //         num=num/2;
+        //     }
+        //     else{
+        //         num=num-1;
+        //         steps++;
+        //     }
+        // }
+        // return steps;
+
+// using recursion
+    
     }
+    public static int steps(int num , int step){
+        if(num == 0 )return step;
+        // understnd stack;
+        else if(num%2==0){
+            return steps(num/2 , step+1);
+        }
+        else{
+            return steps(num-1,step+1);
+        }
+    }
+
 
     
 }
