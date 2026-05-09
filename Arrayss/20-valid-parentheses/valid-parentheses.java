dd1a7145917ec4e0037456134bf4d5b15dad88c6
anudeep4488 <anudeepj04@gmail.com>
2026-05-10 00:28:15 +0530

    Time: 3 ms (87.66%) | Memory: 43.2 MB (72.15%) - LeetSync

--- Arrayss/20-valid-parentheses/valid-parentheses.java
+++ Arrayss/20-valid-parentheses/valid-parentheses.java
@@ -21,13 +21,13 @@ class Solution {
                 if(st.isEmpty()) {
                     return false;
                 }
-
+               // we actually pop it and compare
                 char top = st.pop();
-
+            // we dont use peek() beace it just to see the top element , actually we wanna pop it and check the top element 
                 // mismatch
-                if((ch == ')' && top != '(') ||
-                   (ch == '}' && top != '{') ||
-                   (ch == ']' && top != '[')) {
+                if((ch == ')' && top!= '(') ||
+                   (ch == '}' && top!= '{') ||
+                   (ch == ']' && top!= '[')) {
 
                     return false;
                 }
