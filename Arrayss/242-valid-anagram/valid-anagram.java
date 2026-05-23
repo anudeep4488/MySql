ec9ac972fcb82744df15b1528afc2102cfee8e26
anudeep4488 <anudeepj04@gmail.com>
2026-05-23 13:47:01 +0530

    Time: 14 ms (27.82%) | Memory: 46.5 MB (32.50%) - LeetSync

--- Arrayss/242-valid-anagram/valid-anagram.java
+++ Arrayss/242-valid-anagram/valid-anagram.java
@@ -3,18 +3,47 @@ class Solution {
     public boolean isAnagram(String s, String t) {
 
         // unicode character means
-        char[] str1 = s.toCharArray();
-        char[] str2 = t.toCharArray();
-        Arrays.sort(str1);
-        Arrays.sort(str2);
 
-        String sorted = new String(str1);
-        String sorted1 = new String(str2);
-
-
-
-        return sorted.equals(sorted1);
-
+        // you cant sort stinngs directly you have to convert to chararry then sort
         
+        // char[] str1 = s.toCharArray(); //O(n)
+        // char[] str2 = t.toCharArray();
+        // // sort menthod is void menthod wont return any thing  just directly sort , dont try to stor in anything
+        
+        // Arrays.sort(str1); //O(n log n)
+        // Arrays.sort(str2);
+        
+
+        // String sorted = new String(str1); // O(n)
+        // String sorted1 = new String(str2);
+
+        // return sorted.equals(sorted1);
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        for(char a : s.toCharArray()){
+            map.put(a , map.getOrDefault(a , 0) + 1 );
+        }
+
+        for(char a : t.toCharArray()){
+            if(!map.containsKey(a)){
+                return false;
+            }
+            else{
+                map.put(a, map.getOrDefault(a , 0)-1);
+            }
+        }
+
+        for(int val : map.values()){
+            if(val > 0){
+                return false;
+            }
+        }
+
+    return true; 
     }
 }
