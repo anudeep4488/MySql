a7f72e40330cb180a0f7aa8239e323ad16bf702c
anudeep4488 <anudeepj04@gmail.com>
2026-06-02 21:30:21 +0530

    Time: 1 ms (19.92%) | Memory: 46.3 MB (95.83%) - LeetSync

--- Arrayss/142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ Arrayss/142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -13,19 +13,60 @@ public class Solution {
     public ListNode detectCycle(ListNode head) {
 
         
+       // unefficient you created extra spce alets to o(n) 
 
-        HashSet<ListNode> list = new HashSet<>();
+       // try to find in constsnt time
+        // HashSet<ListNode> list = new HashSet<>();
+
+        // ListNode node = head;
+        // while(node != null){
+        //     if(list.contains(node)){
+        //         return node;
+        //     }
+        //     list.add(node);
+        //     node = node.next;
+        // }
+        // return null;
 
 
-        ListNode node = head;
-        while(node != null){
-            if(list.contains(node)){
-                return node;
-            }
-            list.add(node);
-            node = node.next;
+        // optimized one
+        // fast and alow pointer
+
+        if(head == null || head.next == null){
+            return null;
         }
+        
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                break;
+            }
+
+
+        }
+        ListNode temp = head;
+
+        while(temp!= null){
+            if(temp==slow){
+                return temp;
+            }
+            temp=temp.next;
+            if(slow!=null){
+                slow=slow.next;
+            };
+            
+        }
+
         return null;
+
+
         
     }
 }
