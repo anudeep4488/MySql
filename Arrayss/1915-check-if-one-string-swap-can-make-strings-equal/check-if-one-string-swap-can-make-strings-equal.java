39be1536425e1783b07bb530acaf68540236ae20
anudeep4488 <anudeepj04@gmail.com>
2026-07-01 17:00:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (57.63%) - LeetSync

--- Arrayss/1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ Arrayss/1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -1,26 +1,61 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-        StringBuilder sb = new StringBuilder();
-        if(s1.equals(s2)) return true;
+        // StringBuilder sb = new StringBuilder();
+        // if(s1.equals(s2)) return true;
 
-        for(int i =0; i<s2.length()-1;i++){
-            for(int j =i+1; j<s2.length();j++){
+        // for(int i =0; i<s2.length()-1;i++){
+        //     for(int j =i+1; j<s2.length();j++){
                 
-                sb.append(s2);
-                char a = sb.charAt(j);
-                sb.setCharAt(j,sb.charAt(i));
-                sb.setCharAt(i,a);
+        //         sb.append(s2);
+        //         char a = sb.charAt(j);
+        //         sb.setCharAt(j,sb.charAt(i));
+        //         sb.setCharAt(i,a);
 
-                if(s1.equals(sb.toString())){
-                    return true;
-                }else{
-                    sb.setLength(0);
+        //         if(s1.equals(sb.toString())){
+        //             return true;
+        //         }else{
+        //             sb.setLength(0);
+        //         }
+
+        //     }
+        // }
+
+        // return false;
+       
+        // Already equal
+        if (s1.equals(s2)) {
+            return true;
+        }
+
+        int first = -1;
+        int second = -1;
+        int count = 0;
+
+        for (int i = 0; i < s1.length(); i++) {
+
+            if (s1.charAt(i) != s2.charAt(i)) {
+
+                count++;
+
+                if (count == 1) {
+                    first = i;
+                } else if (count == 2) {
+                    second = i;
+                } else {
+                    return false; // More than 2 mismatches
                 }
-
             }
         }
 
-        return false;
+        // Exactly one mismatch cannot be fixed by one swap
+        if (count != 2) {
+            return false;
+        }
+
+        // Check if swapping fixes the strings
+        return s1.charAt(first) == s2.charAt(second) &&
+               s1.charAt(second) == s2.charAt(first);
+    
         
     }
 }
