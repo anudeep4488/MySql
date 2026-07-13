c518da56161f407c23084a03f8da225b9f96f4af
anudeep4488 <anudeepj04@gmail.com>
2026-07-13 17:11:02 +0530

    Time: 2 ms (90.04%) | Memory: 46.3 MB (65.17%) - LeetSync

---
+++ Arrayss/983-validate-stack-sequences/validate-stack-sequences.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        
+        // int j = 0;
+        // // here for wont work for all test cases because we have to check previosy elemey also  remember is previosu elemet is matches  then wwe have to think about stcak
+        // int i =0;
+        // while(i<pushed.length){
+
+        //     while(i>=0 && j<popped.length && pushed[i] == popped[j]){
+        //         pushed[i] = -1;
+        //         j++;
+        //         while(i>=0 && pushed[i-1] == -1){
+        //             i--;
+        //         }
+        //         i--;
+        //     }
+        //     i++;
+        // }
+        // for(int k = pushed.length -1;k>=0;k--){
+        //     if(pushed[k] == -1) continue ;
+        //     else if(pushed[k] != popped[k]){
+        //         return false;
+        //     }
+        //     else {
+        //         j++;
+        //     }
+        // }
+        // return true;
+        int j = 0;
+        Stack<Integer> st = new Stack<Integer>();
+        
+        for(int i =0 ; i<=pushed.length;i++){
+
+            while(!st.isEmpty() && st.peek() == popped[j]){
+                st.pop();
+                j++;
+            }
+            if(i < pushed.length){
+                st.push(pushed[i]);
+
+            }
+        }   
+        return  st.isEmpty();    }
+}
